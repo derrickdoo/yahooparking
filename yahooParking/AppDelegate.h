ba84882c4c1a9b6528b6ee69c2dc8f77a92bcfa9
derricko <derricko@yahoo-inc.com>
2014-07-24 15:55:44 -0700

    Initial Commit

---
+++ yahooParking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yahooParking
+//
+//  Created by Derrick Or on 7/24/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yahooParking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yahooParking
+//
+//  Created by Derrick Or on 7/24/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
